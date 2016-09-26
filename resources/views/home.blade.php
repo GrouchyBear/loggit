0c331b254b26281afc5c64f36d39f80ce3cc1445
GrouchyBear <benjaminshlack@gmail.com>
2016-09-26 19:50:59 +0100

    added more markup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,15 @@
 
         <div class="lg--prof-pic">
         </div>
-        <div class="lg--prof-info">
-          <h3>{{Auth::name()}}</h3>
+        <div class="col m6">
+            <div class="lg--prof-info">
+            <h3>{{}}<a href="#">Edit Name</a> </h3>
+                <ul>
+                    <li>Course: {{}}</li>
+                    <li>Attendance: {{}}</li>
+                    <li>Assignments due: {{}}</li>
+                </ul>
+            </div>
         </div>
 
     </div>
