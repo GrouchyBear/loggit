017250b74a39f78396c9ba9e073b88f59d8b9d8d
GrouchyBear <benjaminshlack@gmail.com>
2016-09-26 19:47:46 +0100

    rev 3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="lg--prof-pic">
         </div>
         <div class="lg--prof-info">
-          <h3>{{$user->name}}</h3>
+          <h3>{{Auth::name()}}</h3>
         </div>
 
     </div>
