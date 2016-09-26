adebc33cacc3304c3c199c069583ffef92b23e6b
GrouchyBear <benjaminshlack@gmail.com>
2016-09-26 19:43:07 +0100

    profile picture, plus username display on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,11 @@
 @section('content')
     <div class="container">
 
-        
+        <div class="lg--prof-pic">
+        </div>
+        <div class="lg--prof-info">
+          <h3>{{Auth::user()}}</h3>
+        </div>
 
     </div>
 
